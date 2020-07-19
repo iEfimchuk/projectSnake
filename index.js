5317edfb44991b4f5bd2d118adf72e00c138fe2c
Иван <efimchukivan@rambler.ru>
2020-07-20 02:16:12 +0300

    Удалил ненужное увеление змейки

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ window.onload = function(event){
     let divGameField = this.document.getElementById('game-field');
 
     let game = new Game(divGameField, 15, 15);
-
-    game.player.addSegment();
 }
