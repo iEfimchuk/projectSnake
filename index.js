99b71e09e28a9868fa174d1eef7e773433c3afd8
Иван <efimchukivan@rambler.ru>
2020-07-22 01:04:52 +0300

    Добавил вывод счета игры

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 window.onload = function(event){
-    let divGameField = this.document.getElementById('game-field');
-
-    let game = new Game(divGameField, 15, 15);
+    let game = new Game(10, 15);
 }
