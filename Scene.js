ff4e371c2f045f2fd87221245ebe2f91fbc1e4fa
Иван <efimchukivan@rambler.ru>
2020-10-07 02:04:12 +0300

    Добавил механизм для хранения имен всех созданных сцен

--- Scene.js
+++ Scene.js
@@ -1,18 +1,30 @@
 const STATES = Object({'CREATED': 0, 'EXECUTING': 1, 'PAUSED': 2, 'STOPPED':3});
 
+let nameList = new Array;
+
 export default class Scene extends EventTarget{
-    constructor(nameID){
+    constructor(nameID, display){
         super();
 
         if(nameID == undefined || nameID == ''){
             nameID = Date.now().toString();
         }
+
+        if(nameList.indexOf(nameID) != -1){
+            nameID += Date.now().toString();
+        }
+
+        nameList.push(nameID);
+
         this._screen = document.createElement('div');
         this._screen.style.position = 'absolute';
         this._screen.style.opacity = 0;
+        this._screen.style.boxSizing = 'border-box';
 
         this._state = STATES.CREATED;
         this._id = nameID;
+
+        display.appendChild(this._screen);
     }
 
     stop(){
