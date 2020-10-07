9102b3802c93f104d41cc09d88f56db73ecd3b69
Иван <efimchukivan@rambler.ru>
2020-10-08 00:38:58 +0300

    Убрал дублирование кода

--- src/Scene.js
+++ src/Scene.js
@@ -28,25 +28,21 @@ export default class Scene extends EventTarget{
     }
 
     stop(){
-        this._screen.style.opacity = 0;
         this.state = STATES.STOPPED;
         this.dispatchEvent(new CustomEvent('OnStop'));
     }
     
     start(){
-        this._screen.style.opacity = 1;
         this.state = STATES.EXECUTING;
         this.dispatchEvent(new CustomEvent('OnStart'));
     }
 
     pause(){
-        this._screen.style.opacity = 0;
         this.state = STATES.PAUSED;
         this.dispatchEvent(new CustomEvent('OnPause'));
     }
     
     reset(){
-        this._screen.style.opacity = 0;
         this.state = STATES.CREATED;
         this.dispatchEvent(new CustomEvent('OnReset'));
     }
@@ -62,6 +58,12 @@ export default class Scene extends EventTarget{
     onKeyboardEvent(event){}
 
     set state(newValue){
+        if(newValue != 1){
+            this._screen.style.opacity = 0;
+        } else {
+            this._screen.style.opacity = 1;
+        }
+
         this.dispatchEvent(new CustomEvent('StateChanging', {detail: {oldState: this._state, newState: newValue,}}));
 
         this._state = newValue;
